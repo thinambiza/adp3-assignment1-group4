9bf14fc095436bf017508b7993da84965237fb5f
PanasheMuinzani <sharondesigns35@gmail.com>
2022-03-31 22:20:11 +0200

    Manager and Team Entities

--- src/main/java/ac/za/cput/entity/Manager.java
+++ src/main/java/ac/za/cput/entity/Manager.java
@@ -1,4 +1,115 @@
 package ac.za.cput.entity;
 
+/**
+ Manager.java
+ Manager Entity
+ Author: Panashe Muinzani (218186568)
+ Date: 31 March 2022
+ */
+
 public class Manager {
+    private String managerId;
+    private String teamId;
+    private String firstName;
+    private int age;
+    private int yearsOfExperience;
+    private int middleName;
+    private int lastName;
+
+    public Manager(Builder Builder) {
+        this.managerId = Builder.managerId;
+        this.teamId = Builder.teamId;
+        this.firstName = Builder.firstName;
+        this.age = Builder.age;
+        this.yearsOfExperience = Builder.yearsOfExperience;
+        this.middleName = Builder.middleName;
+        this.lastName = Builder.lastName;
+    }
+
+    public String getManagerId() {
+        return managerId;
+    }
+
+    public String getTeamId() {
+        return teamId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public int getMiddleName() {
+        return middleName;
+    }
+
+    public int getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "managerId='" + managerId + '\'' +
+                ", teamId='" + teamId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", age=" + age +
+                ", yearsOfExperience=" + yearsOfExperience +
+                ", middleName=" + middleName +
+                ", lastName=" + lastName +
+                '}';
+    }
+
+    public static class Builder {
+        private String managerId;
+        private String teamId;
+        private String firstName;
+        private int age;
+        private int yearsOfExperience;
+        private int middleName;
+        private int lastName;
+
+        public Builder setManagerId(String managerId) {
+            this.managerId = managerId;
+            return this;
+        }
+
+        public Builder setTeamId(String teamId) {
+            this.teamId = teamId;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder setYearsOfExperience(int yearsOfExperience) {
+            this.yearsOfExperience = yearsOfExperience;
+            return this;
+        }
+
+        public Builder setMiddleName(int middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+
+        public Builder setLastName(int lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+    }
 }
+
--- src/main/java/ac/za/cput/entity/Team.java
+++ src/main/java/ac/za/cput/entity/Team.java
@@ -1,4 +1,131 @@
 package ac.za.cput.entity;
 
+/**
+ Team.java
+ Team Entity
+ Author: Panashe Muinzani (218186568)
+ Date: 31 March 2022
+ */
+
 public class Team {
+    private String teamId;
+    private String teamName;
+    private String venueId;
+    private int numberOfTrophies;
+    private int yearEstablished;
+    private Manager manager;
+    private Player captain;
+    private Player players;
+
+    public Team(Builder Builder) {
+        this.teamId = Builder.teamId;
+        this.teamName = Builder.teamName;
+        this.venueId = Builder.venueId;
+        this.numberOfTrophies = Builder.numberOfTrophies;
+        this.yearEstablished = Builder.yearEstablished;
+        this.manager = Builder.manager;
+        this.captain = Builder.captain;
+        this.players = Builder.players;
+    }
+
+    public String getTeamId() {
+        return teamId;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public String getVenueId() {
+        return venueId;
+    }
+
+    public int getNumberOfTrophies() {
+        return numberOfTrophies;
+    }
+
+    public int getYearEstablished() {
+        return yearEstablished;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public Player getCaptain() {
+        return captain;
+    }
+
+    public Player getPlayers() {
+        return players;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "teamId='" + teamId + '\'' +
+                ", teamName='" + teamName + '\'' +
+                ", venueId='" + venueId + '\'' +
+                ", numberOfTrophies=" + numberOfTrophies +
+                ", yearEstablished=" + yearEstablished +
+                ", manager=" + manager +
+                ", captain=" + captain +
+                ", players=" + players +
+                '}';
+    }
+
+    public static class Builder {
+        private String teamId;
+        private String teamName;
+        private String venueId;
+        private int numberOfTrophies;
+        private int yearEstablished;
+        private Manager manager;
+        private Player captain;
+        private Player players;
+
+        public Builder setTeamId(String teamId) {
+            this.teamId = teamId;
+            return this;
+        }
+
+        public Builder setTeamName(String teamName) {
+            this.teamName = teamName;
+            return this;
+        }
+
+        public Builder setVenueId(String venueId) {
+            this.venueId = venueId;
+            return this;
+        }
+
+        public Builder setNumberOfTrophies(int numberOfTrophies) {
+            this.numberOfTrophies = numberOfTrophies;
+            return this;
+        }
+
+        public Builder setYearEstablished(int yearEstablished) {
+            this.yearEstablished = yearEstablished;
+            return this;
+        }
+
+        public Builder setManager(Manager manager) {
+            this.manager = manager;
+            return this;
+        }
+
+        public Builder setCaptain(Player captain) {
+            this.captain = captain;
+            return this;
+        }
+
+        public Builder setPlayers(Player players) {
+            this.players = players;
+            return this;
+        }
+    }
 }
+
+
+
+
