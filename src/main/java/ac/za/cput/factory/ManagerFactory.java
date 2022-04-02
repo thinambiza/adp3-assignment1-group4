afda5cda994086afaf3b1e9774e2a17b6e935394
PanasheMuinzani <sharondesigns35@gmail.com>
2022-04-02 20:35:26 +0200

    Manager & Team Factory

--- src/main/java/ac/za/cput/factory/ManagerFactory.java
+++ src/main/java/ac/za/cput/factory/ManagerFactory.java
@@ -1,4 +1,27 @@
+/**
+ ManagerFactory.java
+ Manager Factory
+ Author: Panashe Muinzani (218186568)
+ Date: 2 April 2022
+ */
+
 package ac.za.cput.factory;
+import ac.za.cput.entity.Manager;
 
 public class ManagerFactory {
+
+    public static Manager createManager(String managerId, String teamId, String firstName,
+                                        int age, int yearsOfExperience, int middleName, int lastName){
+
+    return new Manager.Builder()
+            .setManagerId(managerId)
+            .setTeamId(teamId)
+            .setFirstName(firstName)
+            .setAge(age)
+            .setYearsOfExperience(yearsOfExperience)
+            .setMiddleName(middleName)
+            .setLastName(lastName)
+            .build();
+
+    }
 }
