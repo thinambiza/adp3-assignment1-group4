2cbb19a25da2fc6960b2ae86bce0523253ba466a
Melisa <217131085@mycput.ac.za>
2022-04-04 14:21:20 +0200

    Tournament and InGameStats Factory

--- src/main/java/ac/za/cput/factory/InGameStatsFactory.java
+++ src/main/java/ac/za/cput/factory/InGameStatsFactory.java
@@ -1,4 +1,43 @@
+/**
+ * Tournament.java
+ * InGame Stats Factory
+ * Author: Melisa Bhixa 217131085
+ * Date: 4 April 2022
+ */
+
 package ac.za.cput.factory;
 
+import ac.za.cput.entity.InGameStats;
+
 public class InGameStatsFactory {
+
+    public static InGameStats createInGameStats(String fixtureId, int numberOfCorners, int numberOfRedCards, int numberOfFreeKicks, int possession,
+                                                int numberOfShotsOnTarget, int numberOfShots, int numberOfPasses, int numberOfGoalsConceded,
+                                                int numberOfGoalsScored, String teamId) {
+        InGameStats InGameStats = null;
+
+        if (fixtureId.trim().isEmpty()) {
+            System.out.println("Please enter Fixture Id");
+
+        } else if (teamId.trim().isEmpty()) {
+            System.out.println("Please enter Team Id");
+
+        } else {
+            InGameStats = new InGameStats.InGameStatsBuilder()
+                    .setFixtureId(fixtureId)
+                    .setNumberOfCorners(numberOfCorners)
+                    .setNumberOfRedCards(numberOfRedCards)
+                    .setNumberOfFreeKicks(numberOfFreeKicks)
+                    .setPossession(possession)
+                    .setNumberOfShotsOnTarget(numberOfShotsOnTarget)
+                    .setNumberOfShots(numberOfShots)
+                    .setNumberOfPasses(numberOfPasses)
+                    .setNumberOfGoalsConceded(numberOfGoalsConceded)
+                    .setNumberOfGoalsScored(numberOfGoalsScored)
+                    .setTeamId(teamId)
+                    .build();
+        }
+        return InGameStats;
+    }
 }
+
