6ed9f957d6818b5b4808b40dbf9354291cb915ce
Thina Mbiza <80575666+thinambiza@users.noreply.github.com>
2022-10-26 22:08:07 +0200

    Update VenueFactory.java

--- src/main/java/ac/za/cput/factory/game/VenueFactory.java
+++ src/main/java/ac/za/cput/factory/game/VenueFactory.java
@@ -1,12 +1,23 @@
 /**
- VenueFactory.java
- Factory for the Venue
- Author: Legiste Ndabashinze (217046207)
- Date: 31 March 2022
+ * Venue Factory
+ * VenueFactory.java
+ * Author: Thina Mbiza 217217095
+ * Date 31 March 2022
  */
-
 package ac.za.cput.factory.game;
 
 
+import ac.za.cput.domain.game.Venue;
+import ac.za.cput.util.Helper;
+
 public class VenueFactory {
+    public static Venue newVenue(String vName, int capacity){
+        String id = "vn-"+ Helper.generateId();
+
+        return new Venue.Builder()
+                .setId(id)
+                .setVenueName(vName)
+                .setCapacity(capacity)
+                .build();
+    }
 }
