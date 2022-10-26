7f3aa5ceaae8897381578c8394649689265d2efd
Thina Mbiza <80575666+thinambiza@users.noreply.github.com>
2022-10-26 22:09:10 +0200

    Update VenueRepository.java

--- src/main/java/ac/za/cput/repository/VenueRepository.java
+++ src/main/java/ac/za/cput/repository/VenueRepository.java
@@ -1,9 +1,15 @@
+/**
+ * Venue Repository
+ * VenueRepository.java
+ * Author: Thina Mbiza 217217095
+ * Date 22 October 2022
+ */
+
 package ac.za.cput.repository;
 
 import ac.za.cput.domain.game.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 @Repository
 public interface VenueRepository extends JpaRepository<Venue, String> {
 }
