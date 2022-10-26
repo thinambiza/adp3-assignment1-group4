b531acef77e361c7bff9ea702fb85b529445062a
PanasheMuinzani <sharondesigns35@gmail.com>
2022-10-26 15:48:22 +0200

    Team updates

--- src/main/java/ac/za/cput/controller/game/TeamController.java
+++ src/main/java/ac/za/cput/controller/game/TeamController.java
@@ -1,5 +1,89 @@
+/**
+ Author: Panashe Muinzani (218186568)
+ */
 package ac.za.cput.controller.game;
+import ac.za.cput.domain.game.Team;
+import ac.za.cput.domain.game.Venue;
+import ac.za.cput.domain.personal.Manager;
+import ac.za.cput.domain.personal.Player;
+import ac.za.cput.service.game.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Set;
+
+@RestController
 public class TeamController {
+
+    @Autowired
+    private TeamService service;
+
+    @GetMapping("/fms/team/all")
+    public List<Team> getAllTeams(){
+        return service.getAllTeams();
+    }
+
+    @GetMapping("/fms/team/{teamId}")
+    public Team getTeamById(@PathVariable String teamId){
+        return (Team) service.getTeamById(teamId);
+    }
+
+    @GetMapping("/fms/tournament/{tournamentId}/team")
+    public List<Team> getTeamByTournamentId(@PathVariable String tournamentId){
+        return service.getTeamByTournamentId(tournamentId);
+    }
+
+    @PostMapping("/fms/team/save")
+    /*public ResponseEntity<Team> save(@RequestBody Team team){
+        service.save(team);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }*/
+    public Team save(@RequestBody Team team){
+        return service.save(team);
+    }
+
+    @DeleteMapping("/fms/team/{teamId}/delete")
+    public ResponseEntity deleteTeamById(@PathVariable String teamId){
+        service.deleteTeamById(teamId);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PutMapping("/fms/manager/{managerId}/assign-to/team/{teamId}")
+    public Team assignManagerToTeam(@PathVariable String managerId, @PathVariable String teamId){
+        return service.assignManagerToTeam(managerId, teamId);
+    }
+
+    @PutMapping("/fms/player/{playerId}/assign-to/team/{teamId}")
+    public Team assignPlayerToTeam(@PathVariable String playerId, @PathVariable String teamId){
+        return service.assignPlayerToTeam(playerId, teamId);
+    }
+
+    @GetMapping("/fms/team/{teamId}/player/all")
+    public Set<Player> getPlayersByTeamId(@PathVariable String teamId){
+        return service.getPlayersByTeamId(teamId);
+    }
+
+    @GetMapping("/fms/team/{teamId}/manager/get")
+    public Set<Manager> getManagerByTeamId(@PathVariable String teamId){
+        return service.getManagerTeamId(teamId);
+    }
+
+    @PutMapping("/fms/venue/{venueId}/assign-to/team/{teamId}")
+    public Team assignVenueToTeam(@PathVariable String venueId, @PathVariable String teamId){
+        return service.assignVenueToTeam(venueId, teamId);
+    }
+
+    @PatchMapping("/fms/team/{teamId}/update-name-to/{teamName}")
+    public void updateTeamDetails(@PathVariable String teamId, @PathVariable String teamName){
+        service.updateTeamNameById(teamId, teamName);
+    }
+
+    @GetMapping("/fms/team/{teamId}/venue/get")
+    public Set<Venue> getVenueByTeamId(@PathVariable String teamId){
+        return service.getVenueByTeamId(teamId);
+    }
 
 }
--- src/main/java/ac/za/cput/service/game/TeamService.java
+++ src/main/java/ac/za/cput/service/game/TeamService.java
@@ -1,5 +1,184 @@
+/**
+ Author: Panashe Muinzani (218186568)
+ */
 package ac.za.cput.service.game;
 
+import ac.za.cput.domain.game.Team;
+import ac.za.cput.domain.game.Tournament;
+import ac.za.cput.domain.game.Venue;
+import ac.za.cput.domain.personal.Manager;
+import ac.za.cput.domain.personal.Player;
+import ac.za.cput.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Streamable;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
 public class TeamService {
+
+    @Autowired
+    private TeamRepository repository;
+    @Autowired
+    private TournamentRepository tournamentRepository;
+
+    @Autowired
+    ManagerRepository managerRepository;
+
+    @Autowired
+    PlayerRepository playerRepository;
+
+    @Autowired
+    VenueRepository venueRepository;
+
+    public Team save(Team team){
+        team.setTitlesWon(0);
+        return repository.save(team);
+    }
+
+    public List<Team> getAllTeams(){
+        List<Team> teams = new ArrayList<>();
+        Streamable.of(repository.findAll())
+                .forEach(teams::add);
+        return teams;
+    }
+
+    public void deleteTeamById(String id){
+        repository.deleteById(id);
+    }
+
+    public List<Team> getTeamByTournamentId(String tournamentId){
+
+        Optional<Tournament> tournament = tournamentRepository.findById(tournamentId);
+
+        List<Team> teams = new ArrayList<>();
+        Streamable.of(tournament.get().getAssignedTeams())
+                .forEach(teams::add);
+        return teams;
+
+    }
+
+    public List<Team> getTeamById(String teamId) {
+        if (null != teamId){
+            return repository.findAllById(Collections.singleton(teamId));
+        }
+        else {
+            return repository.findAll();
+        }
+    }
+
+    public Team assignManagerToTeam(String managerId, String teamId){
+
+        Set<Manager> managerSet = null;
+
+        Team team = repository.findById(teamId).get();
+        Manager manager = managerRepository.findById(managerId).get();
+
+
+        // Prevents manager from managing two or more teams and the same team again.
+        if (team.getTeamManager().size() == 0){
+
+            if (manager.getTeamSet().size() == 0){
+
+                managerSet = team.getTeamManager();
+                managerSet.add(manager);
+                team.setTeamManager(managerSet);
+
+                return repository.save(team);
+            }
+            else return null;
+        }
+
+        else return null;
+
+    }
+
+    public Team assignPlayerToTeam(String playerId, String teamId){
+        Set<Player> playerSet = null ;
+
+        Team team = repository.findById(teamId).get();
+        Player player = playerRepository.findById(playerId).get();
+
+
+        // prevents player from being added to multiple teams and the same team again.
+        if (player.getTeam().size() == 0){
+
+            if (team.getTeamPlayers().size()<=team.getMaxPlayers()){
+
+                playerSet = team.getTeamPlayers();
+                playerSet.add(player);
+                team.setTeamPlayers(playerSet);
+
+                return repository.save(team);
+
+            }
+
+            else return null;
+
+        }
+
+        else return null;
+
+    }
+
+    public Team assignVenueToTeam(String venueId, String teamId){
+
+        Set<Venue> venueSet = null;
+
+        Team team = repository.findById(teamId).get();
+        Venue venue = venueRepository.findById(venueId).get();
+
+
+        // prevents Venue from being added to multiple teams and the same team again.
+        if (team.getTeamHomeVenue().size() == 0){
+
+            if (venue.getTeam().size() == 0){
+
+                venueSet = team.getTeamHomeVenue();
+                venueSet.add(venue);
+                team.setTeamHomeVenue(venueSet);
+
+                return repository.save(team);
+            }
+            else return null;
+        }
+
+        else return null;
+
+    }
+
+    public Set<Player> getPlayersByTeamId(String teamId){
+
+        Team team = repository.findById(teamId).get();
+
+        Set<Player> players = team.getTeamPlayers();
+
+        return players;
+    }
+
+    public Set<Manager> getManagerTeamId(String teamId){
+
+        Team team = repository.findById(teamId).get();
+        Set<Manager> manager = team.getTeamManager();
+
+        return manager;
+    }
+
+    public Set<Venue> getVenueByTeamId(String teamId){
+        Team team = repository.findById(teamId).get();
+
+        Set<Venue> venue = team.getTeamHomeVenue();
+
+        return venue;
+
+    }
+
+    public void updateTeamNameById(String teamId, String teamName){
+        Team team = repository.findById(teamId).get();
+        team.setTeamName(teamName);
+        repository.save(team);
+    }
+
 
 }
