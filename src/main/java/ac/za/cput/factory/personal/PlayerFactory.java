68c84b2a04be91bbf56f16de8005098e03a5f683
Thina Mbiza <80575666+thinambiza@users.noreply.github.com>
2022-10-26 22:05:37 +0200

    Update PlayerFactory.java

--- src/main/java/ac/za/cput/factory/personal/PlayerFactory.java
+++ src/main/java/ac/za/cput/factory/personal/PlayerFactory.java
@@ -6,7 +6,25 @@
  */
 
 
-package ac.za.cput.factory.personal;
+package ac.za.cput.factory;
+
+import ac.za.cput.entity.Player;
+import ac.za.cput.util.Helper;
+
 
 public class PlayerFactory {
+    public static Player newPlayer(String fName, String mName, String lName, int age, String position, int positionNumber, int totalGoals, int totalAssistsMade){
+        String id = "mn-"+ Helper.generateId();
+        return new Player.Builder()
+                .setId(id)
+                .setFirstName(fName)
+                .setMiddleName(mName)
+                .setLastName(lName)
+                .setAge(age)
+                .setPosition(position)
+                .setPositionNumber(positionNumber)
+                .setTotalGoalsScored(totalGoals)
+                .setTotalAssistsMade(totalAssistsMade)
+                .build();
+    }
 }
