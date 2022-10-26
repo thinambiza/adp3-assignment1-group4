fb527ddf70f9cb1a80599e264db2d22b5149d049
legiste99 <legistendabashinze@gmail.com>
2022-10-26 08:17:43 +0200

    All Fixture and TeamStats updated

--- src/main/java/ac/za/cput/domain/game/Fixture.java
+++ src/main/java/ac/za/cput/domain/game/Fixture.java
@@ -7,6 +7,76 @@ Date: 30 March 2022
 
 package ac.za.cput.domain.game;
 
+import ac.za.cput.domain.info.GameStats;
+import ac.za.cput.domain.info.PlayerStats;
+import ac.za.cput.domain.personal.Referee;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "fixture")
 public class Fixture {
+    @Id
+    @Column(name = "fixture_id", nullable = false)
+    private String id;
+    private int status; // 0=create, 1=pending(when only 1 team is added) 2=in progress, 3=complete
+    private final int maxTeams = 2;
+    String teamAId, teamBid;
+    private String teamAName, teamBName;
+
+
+    /*
+            Many to many but only two teams are required in a Fixture, Client Side should make
+            sure no more than 2 teams are added to a Fixture, otherwise that would just be weird.
+    * */
+    @ManyToMany
+    @JoinTable(name = "fixture_teams",
+            joinColumns = @JoinColumn(name = "fixture_id"),
+            inverseJoinColumns = @JoinColumn(name = "team_id")
+    )
+    private Set<Team> fixtureTeams = new HashSet<>();
+
+
+    //Unidirectional - Player Stats
+    //This should be a ManyToOne Relationship
+    //When a Team is added to a fixture, PlayerStat is created for each player in that team.
+    @ManyToMany
+    @JoinTable(name = "fixture_player_stats",
+            joinColumns = @JoinColumn(name = "fixture_id"),
+            inverseJoinColumns = @JoinColumn(name = "player_stats_id")
+    )
+    private Set<PlayerStats> fixturePlayerStats = new HashSet<>();
+
+    //Unidirectional - Game Stats
+    // Created when a team is assigned to a fixture. A fixture should/will only have two GameStats, one for each team in the fixture
+    @ManyToMany
+    @JoinTable(name = "fixture_game_stats",
+            joinColumns = @JoinColumn(name = "fixture_id"),
+            inverseJoinColumns = @JoinColumn(name = "game_stats_id")
+    )
+    private Set<GameStats> fixtureGameStats = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(name = "fixture_referee",
+            joinColumns = @JoinColumn(name = "fixture_id"),
+            inverseJoinColumns = @JoinColumn(name = "referee_id")
+    )
+    private List<Referee> fixtureReferee = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "fixture_venue",
+            joinColumns = @JoinColumn(name = "fixture_id"),
+            inverseJoinColumns = @JoinColumn(name = "venue_id")
+    )
+    private List<Venue> fixtureVenue = new ArrayList<>();
+
 
 }
--- src/main/java/ac/za/cput/domain/info/TeamStats.java
+++ src/main/java/ac/za/cput/domain/info/TeamStats.java
@@ -1,11 +1,121 @@
 /**
  Log.java
- Entity for the Log
- Author: Zuko Fukula (217299911)
+ Entity for the TeamStats
+ Author: Legiste Ndabashinze (217046207)
  Date: 31 March 2022
  */
 
 package ac.za.cput.domain.info;
 
+import ac.za.cput.domain.game.Tournament;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "team_stats")
 public class TeamStats {
+    @Id
+    @Column(name = "id", nullable = false)
+    private String id;
+    private String teamId;
+    private String teamName;
+    private int points;
+    private int gamesPlayed;
+    private int goalsFor;
+    private int goalsAgainst;
+    private int goalDifference;
+
+    // Not Necessary
+    @JsonIgnore
+    @ManyToMany(mappedBy = "tournamentTeamStats")
+    private Set<Tournament> tournaments = new HashSet<>();
+
+    public TeamStats(){}
+
+    public TeamStats(Builder builder) {
+        this.id = builder.id;
+        this.teamId = builder.teamId;
+        this.teamName = builder.teamName;
+        this.points = builder.points;
+        this.gamesPlayed = builder.gamesPlayed;;
+        this.goalsFor = builder.goalsFor;
+        this.goalsAgainst = builder.goalsAgainst;
+        this.goalDifference = builder.goalDifference;
+    }
+
+    public static class Builder{
+
+        private String id;
+        private String teamId;
+        private String teamName;
+        private int points;
+        private int gamesPlayed;
+        private int goalsFor;
+        private int goalsAgainst;
+        private int goalDifference;
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setTeamId(String teamId) {
+            this.teamId = teamId;
+            return this;
+        }
+
+        public Builder setTeamName(String teamName) {
+            this.teamName = teamName;
+            return this;
+        }
+
+        public Builder setPoints(int points) {
+            this.points = points;
+            return this;
+        }
+
+        public Builder setGamesPlayed(int gamesPlayed) {
+            this.gamesPlayed = gamesPlayed;
+            return this;
+        }
+
+        public Builder setGoalsFor(int goalsFor) {
+            this.goalsFor = goalsFor;
+            return this;
+        }
+
+        public Builder setGoalsAgainst(int goalsAgainst) {
+            this.goalsAgainst = goalsAgainst;
+            return this;
+        }
+
+        public Builder setGoalDifference(int goalDifference) {
+            this.goalDifference = goalDifference;
+            return this;
+        }
+
+        public Builder copy(TeamStats teamStats){
+            this.id = teamStats.id;
+            this.teamId = teamStats.teamId;
+            this.teamName = teamStats.teamName;
+            this.points = teamStats.points;
+            this.gamesPlayed = teamStats.gamesPlayed;
+            this.goalsFor = teamStats.goalsFor;
+            this.goalsAgainst = teamStats.goalsAgainst;
+            this.goalDifference = teamStats.goalDifference;
+            return this;
+        }
+
+        public TeamStats build(){
+            return new TeamStats(this);
+        }
+
+    }
 }
